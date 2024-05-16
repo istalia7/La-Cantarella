75f55c2c238119369875745122d09c59894f499e
istalia7 <alexis.perrotin02@gmail.com>
2024-05-16 12:59:25 +0200

    home and css update

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,51 @@
  * which should already be in your base.html.twig.
  */
 import "./styles/app.css";
-import "flowbite";
+// import "flowbite";
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 5000);
+  }
+}
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+
+var button1 = document.querySelector("#next");
+button1.addEventListener("click", () => {
+  nextSlide();
+});
+
+var button2 = document.querySelector("#prev");
+button2.addEventListener("click", () => {
+  prevSlide();
+});
+
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
 
 console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉");
