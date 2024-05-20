407da623dd69dc931828f6d21ef343363c46a105
istalia7 <alexis.perrotin02@gmail.com>
2024-05-20 17:24:23 +0200

    responsive update

---
+++ public/js/navbar.js
@@ -0,0 +1,7 @@
+let navbuton = document.querySelector(".displayMenu");
+let nav = document.querySelector(".navbar");
+let menu = document.querySelector("#menuItems");
+navbuton.addEventListener("click", () => {
+  nav.classList.toggle("navbar");
+  menu.classList.toggle("displayMenuItems");
+});
